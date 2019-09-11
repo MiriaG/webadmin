c8a98b66d4e824432653c0889306e13c8b544122
aluno <aluno@myco.lxd>
2019-09-11 18:51:45 +0000

    update

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask , render_template
+
+app=Flask(__name__)
+
+
+@app.route ("/") #MAPEANDO AS FUNÇÕES 
+def index ():
+	return "CHEGA DE ICS"
+
+@app.route ("/pagina") #MAPEANDO AS FUNÇÕES 
+def pagina ():
+	return render_template ("index.html")
+
+@app.route ("/test") #MAPEANDO AS FUNÇÕES 
+def teste ():
+	return "<input type='submit' value='intervalo'></imput>"
+
+app.run (debug=True, host="0.0.0") #MODO DE DEPURAÇÃO, ATUALIZA SOZINHO
